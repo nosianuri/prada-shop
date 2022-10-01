3515070db1b30b13681a30ae518548c435576504
nosianuri <nurinosia@gmail.com>
2022-10-01 20:10:40 +0600

    update banner and style

--- js/script.js
+++ js/script.js
@@ -118,13 +118,8 @@ var swiper = new Swiper(".review-slider", {
   },
 });
 
-var swiper = new Swiper(".banner-swiper", {
+var swiper = new Swiper(".banner-Swiper", {
   spaceBetween: 30,
-  effect: "fade",
-  navigation: {
-    nextEl: ".swiper-button-next",
-    prevEl: ".swiper-button-prev",
-  },
   pagination: {
     el: ".swiper-pagination",
     clickable: true,
